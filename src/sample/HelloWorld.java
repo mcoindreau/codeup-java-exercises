dba5954b25f8c30468717584288dd3f099cad625
M Coindreau <mariannacoindreau@gmail.com>
2017-09-18 10:00:17 -0500

    Initial commit

---
+++ src/sample/HelloWorld.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class HelloWorld { //every Java app needs this
+    // needs psvm
+    //Entry point
+    public static void main(String[] bananas){ //**Java must have this main method. Nothing can change except "bananas" should use "args".
+        //type psvm and "Enter"
+        System.out.println("Hi");
+
+        System.out.print("Hello World");
+        //type sout and "Enter" adds ln to add new line
+
+    }
+}
