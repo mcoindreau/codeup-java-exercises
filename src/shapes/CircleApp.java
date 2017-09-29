6abedc523ac9f65f76c1322dcfcb2f8f9b41d4ba
M Coindreau <mariannacoindreau@gmail.com>
2017-09-29 11:26:10 -0500

    completed circleapp

---
+++ src/shapes/CircleApp.java
@@ -0,0 +1,17 @@
+package Shapes;
+import util.Input;
+
+public class CircleApp {
+    public static void main(String[] args) {
+
+        Input input = new Input();
+
+        System.out.println("Enter the radius:");
+
+//        Circle myCircle = new Circle(input.getDouble(5, 5));
+//
+//        System.out.println("The area is " + myCircle.getArea());
+//        System.out.println("The circumference is " + myCircle.getCircumference());
+
+    }
+}
