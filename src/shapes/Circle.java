be207fc027a29109eb0ba8d22e2713c6747e3d71
M Coindreau <mariannacoindreau@gmail.com>
2017-09-29 11:48:25 -0500

    Forcing filename change

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,9 +1,9 @@
 package shapes;
 
-public class CircleTest {
+public class Circle {
     private double radius;
 
-    public CircleTest(double radius) {
+    public Circle(double radius) {
         this.radius = radius;
     }
 
