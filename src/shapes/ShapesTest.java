ea813bf52789d5406cdea757e7ff233aa4dffb27
M Coindreau <mariannacoindreau@gmail.com>
2017-09-29 10:46:55 -0500

    completed abstract exercise

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -3,15 +3,27 @@ package shapes;
 public class ShapesTest {
 
     public static void main(String[] args) {
-    
-        Rectangle box1 = new Rectangle(4, 5);
+
+        Measurable box1 = new Rectangle(4, 5);
 
         System.out.println("box1.getPerimeter() = " + box1.getPerimeter());
         System.out.println("box1 = " + box1.getArea() + "\n");
 
-        Rectangle box2 = new Square(5); //data type looking for is superclass on left side
+        Measurable box2 = new Square(5);
 
         System.out.println("box2 = " + box2.getPerimeter());
-        System.out.println("box2.getArea() = " + box2.getArea());
+        System.out.println("box2.getArea() = " + box2.getArea() + "\n\n");
+
+        Measurable myShape = new Rectangle(4, 5);
+
+        System.out.println("myShapeRectangle.getPerimeter() = " + myShape.getPerimeter());
+        System.out.println("myShapeRectangle = " + myShape.getArea() + "\n");
+
+        myShape = new Square(5);
+
+        System.out.println("myShape2 Square = " + myShape.getPerimeter());
+        System.out.println("myShape2Square.getArea() = " + myShape.getArea());
     }
+
+
 }
